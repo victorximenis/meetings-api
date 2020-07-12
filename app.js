7a9d601329252c4105bd43c851b123b8a89c7f5c
Victor Ximenis <victorximenis@gmail.com>
2020-07-12 03:03:14 -0300

    Tratamento de erro 404

--- app.js
+++ app.js
@@ -14,4 +14,20 @@ app.use(cookieParser());
 
 app.use('/users', usersRouter);
 
+// Tratamento de erro quando não encontrar rota
+app.use((req, res, next) => {
+    const erro = new Error('Recurso não encontrado');
+    erro.status = 404;
+    next(erro);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    });
+});
+
 module.exports = app;
